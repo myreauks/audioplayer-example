b6829d5cd3b1926d3abc40fb87e63bf6775861c5
Miro Virta <miro.virta@kone.com>
2023-10-21 12:29:13 +0300

    Initial Commit

---
+++ audioplayer-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  audioplayer-example
+//
+//  Created by Miro Virta on 21.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ audioplayer-example/audioplayer_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioplayer_exampleApp.swift
+//  audioplayer-example
+//
+//  Created by Miro Virta on 21.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct audioplayer_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
